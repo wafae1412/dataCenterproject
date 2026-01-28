0ccecb45c372abc29949dd44dac83d9b09e7a005
Wafae Achahbar <wafaeachahbar2@gmail.com>
2026-01-28 22:59:57 +0100

    Update project after modification

---
+++ app/Http/Controllers/ReservationStatusChanged.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Reservation;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ReservationStatusChanged extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $reservation;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Reservation $reservation)
+    {
+        $this->reservation = $reservation;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Mise à jour de votre réservation - DataCenter Hub')
+                    ->view('emails.reservation_status_changed');
+    }
+}
